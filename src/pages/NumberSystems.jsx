c30d954f9db69026c061053e5e44dc2e7cd64b52
Tima Gezalov <t.gezalov98@gmail.com>
2024-08-26 16:17:41 -0500

    latest commit fix

--- src/pages/NumberSystems.jsx
+++ src/pages/NumberSystems.jsx
@@ -14,7 +14,7 @@ export default function NumberSystems() {
     const startConversion = () => {
         switch (inputTypeRef.current.value) {
             case "1": // Decimal
-                if (isNaN(userInput) || userInput.includes("-")) {
+                if (isNaN(userInput) || userInput.includes("-") || userInput.includes(".")) {
                     setInvalidInput(true);
                     break;
                 }
