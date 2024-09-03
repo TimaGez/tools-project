5104902cdfe18a0c3aeeae179b417673aaa00d3a
Tima Gezalov <t.gezalov98@gmail.com>
2024-09-03 11:00:42 -0500

    finished stats calc :o

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <>
             <div id='home-heads'>
                 <h1 id="home-head">Tools Project</h1>
-                <h2 id="home-head2">This is a personal project aiming to automate basic calculations in various subjects.</h2>
+                <h2 id="home-head2">This is a personal project, developed by <a href="https://www.github.com/timagez">Tima Gezalov</a> and <a href="https://github.com/starlessknights">Ryan</a>, aiming to automate basic calculations in various subjects.</h2>
                 <h3 id="home-head3">The current resources available are: </h3>
             </div>
             <Link to={"/number-systems"}>
